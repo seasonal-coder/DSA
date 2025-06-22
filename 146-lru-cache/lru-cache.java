9d6666cc30c0ebad069af7758a1e9615b4abd32c
seasonal-coder <166492529+seasonal-coder@users.noreply.github.com>
2025-06-22 22:20:47 +0530

    Time: 45 ms (69.88%) | Memory: 111.5 MB (92.09%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,67 +1,24 @@
-class LRUCache {
-
-    class Node {
-        int key, value;
-        Node prev, next;
-        Node(int k, int v) {
-            key = k;
-            value= v;
-        }
-    }
-
+class LRUCache extends LinkedHashMap<Integer, Integer> {
     private int capacity;
-    private Map<Integer, Node> map;
-    private Node head, tail;
-
 
     public LRUCache(int capacity) {
+        super(capacity, 0.75f, true); // true = access-order
         this.capacity = capacity;
-        map = new HashMap<>();
-        head = new Node(0,0);
-        tail = new Node(0,0);
-        head.next = tail;
-        tail.prev = head;
-        
     }
 
-    private void remove(Node node){
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-
-    private void insertToFront(Node node){
-        node.next = head.next;
-        head.next.prev = node;
-        head.next= node;
-        node.prev= head;
-
-    }
-    
     public int get(int key) {
-        if(!map.containsKey(key)) return -1;
-        Node node = map.get(key);
-        remove(node);
-        insertToFront(node);
-
-        return node.value;
-        
+        return super.getOrDefault(key, -1);
     }
-    
-    public void put(int key, int value) {
-        if(map.containsKey(key)){
-            remove(map.get(key));
-        }
-        Node node = new Node(key, value);
-        insertToFront(node);
-        map.put(key,node);
 
-        if(map.size()>capacity){
-            Node lru = tail.prev;
-            remove(lru);
-            map.remove(lru.key);
-        }
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
     }
 }
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
