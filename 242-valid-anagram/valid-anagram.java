4b1fe9af81153d5f4fa0d67cb2f5d563da73c5fa
seasonal-coder <166492529+seasonal-coder@users.noreply.github.com>
2025-06-28 18:41:12 +0530

    Time: 4 ms (77.72%) | Memory: 45 MB (11.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,19 +3,10 @@ class Solution {
         
     if (s.length() != t.length()) return false;
 
-    int[] count = new int[26];  // only for lowercase letters
-
-    for (int i = 0; i < s.length(); i++) {
-        count[s.charAt(i) - 'a']++;
-        count[t.charAt(i) - 'a']--;
-    }
-
-    for (int c : count) {
-        if (c != 0) return false;
-    }
-
-    return true;
-    
+char[] sArr = s.toCharArray(), tArr = t.toCharArray();
+Arrays.sort(sArr);
+Arrays.sort(tArr);
+return Arrays.equals(sArr, tArr);
 
         
     }
