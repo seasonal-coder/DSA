4214ca76c97a0fb8af55ea04b0d2afb4dca98ab7
seasonal-coder <166492529+seasonal-coder@users.noreply.github.com>
2025-09-16 16:36:30 +0530

    Time: 1 ms (71.11%) | Memory: 45.7 MB (47.56%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int r = matrix.length, c = matrix[0].length;
+        boolean col0 = false;
+
+        for(int i=0; i<r; i++){
+            if(matrix[i][0] == 0) col0 = true;
+            for(int j =1; j<c; j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+
+        for(int i =1; i<r; i++){
+            for(int j =1; j<c; j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(matrix[0][0]==0){
+            for(int j=0; j<c; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(col0){
+            for(int i=0; i<r; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        
+    }
+}
